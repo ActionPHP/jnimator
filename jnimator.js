4287f28db8bc08c71a1b6389a1a69bdb7f31a8fb
ActionPHP <giosis2e@yahoo.co.uk>
2013-02-06 22:14:20 +0000

    Update jnimator.js

--- jnimator.js
+++ jnimator.js
@@ -1,6 +1,6 @@
 /****************************************************
  * 
- * ActionPHP jNimator v 1.0
+ * ActionPHP jNimator
  * (c)2013 ActionPHP Ltd
  * 
  * **************************************************/
@@ -56,6 +56,26 @@
   		
   		break;  		
   		
+  			
+  		case 'in':
+  		
+  			this.delay( delay ).fadeIn( duration, function() { 
+  		
+  				if( typeof options.callback == "function" ){ options.callback.call(); 	 }
+  				 
+  		 } );
+  		
+  		break;
+  		
+  		case 'out':
+  		
+  			this.delay( delay ).fadeOut( duration, function() { 
+  		
+  				if( typeof options.callback == "function" ){ options.callback.call(); 	 }
+  				 
+  		 } );
+  		
+  		break;  
   		
   		
   	}
@@ -70,3 +90,79 @@
 
   };
 })( jQuery );
+
+
+ 
+ //var stepDelay = 0;
+ //x = 0;
+
+	
+ 	function animateThis( step, delay ){
+ 		
+ 			sequence = window.sequence;
+ 			
+ 			nextStepPosition = step;
+ 			nextKey = 'step_' + nextStepPosition;
+ 			nextStep = sequence[nextKey];
+ 		
+ 		setTimeout( function() {
+ 			
+ 			executeStep( nextStep );
+ 			stepDelay = Number(sequence[nextKey].delay);
+ 			step++;
+ 			
+ 			if ( step > window.totalSteps ) { return }
+ 			animateThis( step, stepDelay );
+ 			
+ 		}, delay);
+ 		
+ 	}
+ 	
+ 	
+ 
+  
+  	function executeStep ( stepObject ){
+  		
+  		for ( var step_key in stepObject ){
+  	 	
+	  	if( stepObject.hasOwnProperty(step_key) ){
+	    
+			    
+	     the_id = String('#' +  stepObject[step_key].id);
+	     the_distance =  stepObject[step_key].distance;
+	     the_duration = Number( stepObject[step_key].duration );
+	     the_direction = stepObject[step_key].direction;
+	     the_callback = stepObject[step_key].callback;
+	     
+	    	jQuery( the_id ).jnimator( {  direction: the_direction, distance: the_distance, duration: the_duration,  callback: function(){ }
+	    		 
+		} );
+		
+		 }
+	  		
+	  	}
+  		
+  		
+  	}
+  	
+  	function animateThis( step, delay ){
+ 		
+ 			sequence = window.sequence;
+ 			
+ 			nextStepPosition = step;
+ 			nextKey = 'step_' + nextStepPosition;
+ 			nextStep = sequence[nextKey];
+ 		
+ 		setTimeout( function() {
+ 			
+ 			executeStep( nextStep );
+ 			stepDelay = Number(sequence[nextKey].delay);
+ 			step++;
+ 			
+ 			if ( step > window.totalSteps ) { return }
+ 			animateThis( step, stepDelay );
+ 			
+ 		}, delay);
+ 		
+ 	}
+ 	
