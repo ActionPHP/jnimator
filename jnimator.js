557e71c5bb30efeb806ff1137e4676e71dce1cf5
ActionPHP <giosis2e@yahoo.co.uk>
2013-02-06 09:42:54 -0800

    Create jnimator.js

---
+++ jnimator.js
@@ -0,0 +1,72 @@
+/****************************************************
+ * 
+ * ActionPHP jNimator
+ * (c)2013 ActionPHP Ltd
+ * 
+ * **************************************************/
+(function( $ ) {
+  $.fn.jnimator = function( options ) {
+  
+    direction = options.direction;
+  	duration = Number( options.duration );
+  	distance = options.distance ;
+  	callback = options.callback;
+  	delay =  Number(options.delay)
+  
+  	switch ( options.direction ){
+  		
+  		case 'up':
+  	
+  			this.delay( delay ).animate( { top: '-='+distance }, duration, function() { 
+  			
+  				if( typeof options.callback == "function" ){ options.callback.call(); }
+  			
+  			} );
+  		
+  		
+  		break;
+  		
+  		case 'down':
+  		
+  			this.delay( delay ).animate( { top: '+='+distance }, duration, function() { 
+  			
+  				if( typeof options.callback == "function" ){ options.callback.call(); }
+  			
+  			} );
+  		
+  		break;
+  		
+  		case 'left':
+  		
+  			this.delay( delay ).animate( { left: '-='+distance }, duration, function() { 	
+  			
+  				if( typeof options.callback == "function" ){ options.callback.call(); }
+  			
+  			} );
+  		
+  		break;
+  		
+  		case 'right':
+  		
+  			this.delay( delay ).animate( { left: '+='+distance }, duration, function() { 
+  		
+  				if( typeof options.callback == "function" ){ options.callback.call(); 	 }
+  				 
+  		 } );
+  		
+  		break;  		
+  		
+  		
+  		
+  	}
+ this.html( options.message );
+     
+     return this.each(function() {        
+
+      
+
+    });
+
+
+  };
+})( jQuery );
